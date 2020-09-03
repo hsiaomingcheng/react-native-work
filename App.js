031c0f3d5b6c755eedd5ff2a6b33a08f3dbca35d
Chris hsiao <hsiao516@gmail.com>
2020-09-03 11:38:19 +0800

    [作業] TextInput元件應用

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [valid, setValid] = useState();
+  const phoneNumber = '0939079170';
+
+  function handleText(e) {
+    setInputValue(e);
+
+    if (phoneNumber !== e) {
+      setValid(false);
+    } else {
+      setValid(true);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.textInput}
+        onChangeText={text => handleText(text)}
+        value={inputValue}
+        keyboardType="numeric"
+        maxLength={10}
+      />
+      <Text>您輸入的手機號碼是：{inputValue}</Text>
+      {
+        inputValue !== '' && <Text>{valid ? '輸入成功！' : '手機輸入錯誤！'}</Text>
+      }
     </View>
   );
 }
@@ -18,4 +40,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    height: 50,
+    width: 300,
+    borderRadius: 0,
+    borderColor: 'darkgray',
+    borderWidth: 5,
+    backgroundColor: 'gray',
+    color: 'white',
+    fontSize: 28,
+    textAlign: 'center'
+  }
 });
