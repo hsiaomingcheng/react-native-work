e031f7eb464e11e83e52a626dc3bf49ccdc3ab53
Chris hsiao <hsiao516@gmail.com>
2020-09-29 00:14:01 +0800

    Revert "Merge branch 'feature/200916/homework_7/chris'"
    This reverts commit 4400151bf61c27f63e6ffaaf73153034737d0c74, reversing
    changes made to a1440b426f2a4b619f260b5ef74b716eb7b82cdb.

--- App.js
+++ App.js
@@ -1,32 +1,54 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from './src/screens/Home.js';
-import HomeDetail from './src/screens/HomeDetail.js';
-import HomeList from './src/screens/HomeList.js';
-
-const Stack = createStackNavigator();
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [valid, setValid] = useState();
+  const phoneNumber = '0939079170';
+
+  function handleText(e) {
+    setInputValue(e);
+
+    if (phoneNumber !== e) {
+      setValid(false);
+    } else {
+      setValid(true);
+    }
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: '來到農村住一晚' }}
-        />
-        <Stack.Screen
-          name="HomeList"
-          component={HomeList}
-          options={{ title: '農村列表' }}
-        />
-        <Stack.Screen
-          name="HomeDetail"
-          component={HomeDetail}
-          options={{ title: '農村' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={text => handleText(text)}
+        value={inputValue}
+        keyboardType="numeric"
+        maxLength={10}
+      />
+      <Text>您輸入的手機號碼是：{inputValue}</Text>
+      {
+        inputValue !== '' && <Text>{valid ? '輸入成功！' : '手機輸入錯誤！'}</Text>
+      }
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    height: 50,
+    width: 300,
+    borderRadius: 0,
+    borderColor: 'darkgray',
+    borderWidth: 5,
+    backgroundColor: 'gray',
+    color: 'white',
+    fontSize: 28,
+    textAlign: 'center'
+  }
+});
